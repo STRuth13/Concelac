4088293b58657e16c8c3bfe747659ef9c0be59fc
STRuth13 <suareztorresr1307@gmail.com>
2018-06-19 13:45:46 -0500

    Cambios prueba Ruth

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 "# Concelac" 
 
+este es un aprueba#
+esta es la rama ruth
--- conexion.php
+++ conexion.php
@@ -8,7 +8,7 @@
 	 	 $pwd = 	'';
 		try {
 		   	$conn = new PDO('mysql:host='.$host.';dbname='.$db, $user, $pwd);
-				// echo 'Connected succesfully.<br>';
+				// echo 'Connected succesfully.<br> ';
 		}
 		catch (PDOException $e) {
 			echo '<p>Cannot connect to database !!</p>';
