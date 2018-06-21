301f7aee15191e335ed56563c6bf0d486c80f29b
STRuth13 <suareztorresr1307@gmail.com>
2018-06-21 10:35:14 -0500

    Ruth

--- hader.php
+++ hader.php
@@ -13,15 +13,7 @@
    		<div id="marca">
    			<h1><span class="resaltado">Planta Lechera</span> Concelac</h1>
    		</div>
-   		<nav>
-   			<ul>
-   				<li class="actual"><a href="index.html">Inicio</a></li>
-   				<li><a href="nosotros.html">Nosotros</a></li>
-   				<li><a href="productos.html">Productos</a></li>
-                   <li><a href="contacto.html">Contacto</a></li>
-                   
-   			</ul>
-           </nav>
+   		
            <footer>
                 <a href="#">Ruth</a>
            </footer>
--- header.php
+++ header.php
@@ -1,20 +1,4 @@
 
-   <header>
-   	<div class="contenedor">
-   		<div id="marca">
-   			<h1><span class="resaltado">Planta Lechera</span> Concelac</h1>
-   		</div>
-   		<nav>
-   			<ul>
-   				<li class="actual"><a href="index.html">Inicio</a></li>
-   				<li><a href="nosotros.php">Nosotros</a></li>
-   				<li><a href="productos.php">Productos</a></li>
-				<li><a href="contacto.php">Contacto</a></li>
-				<li><a href="login.php">Sign In</a></li>
-   			</ul>
-   		</nav>
-   	</div>
-   </header>
    <?php require_once('conexion.php'); 
    $conn = dbConexion(); ?>
    
